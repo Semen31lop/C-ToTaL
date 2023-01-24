21253b688085c308531e86d163f4f639cc85455d
semen <enota31@bk.rum>
2023-01-24 21:56:50 +0300

    второй шаг

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿string[] array1 = new string[4] {"branch", "checkout","commit","yes"};
+ string[] arr = new string[array1.Length];
+
+ void NewArray (string[] arra1, string [] arr)
+ {
+    int count= 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length<=3)
+        {
+            count++;
+            arr[count]=arra1[i];
+        }
+    }
+ }
+
